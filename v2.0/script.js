13016dd3e0cc0cca99fcdbb298db2fe0c96786d0
dmbcjl <34770126+dmbcjl@users.noreply.github.com>
2020-07-14 19:38:03 +0800

    Add files via upload

---
+++ v2.0/script.js
@@ -0,0 +1,9 @@
+(function() {
+  $('.hider').click(function() {
+    return $(this).parent('.message').removeClass('blur');
+  });
+
+}).call(this);
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiPGFub255bW91cz4iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7RUFBQSxDQUFBLENBQUUsUUFBRixDQUFXLENBQUMsS0FBWixDQUFrQixRQUFBLENBQUEsQ0FBQTtXQUNoQixDQUFBLENBQUUsSUFBRixDQUFJLENBQUMsTUFBTCxDQUFZLFVBQVosQ0FBdUIsQ0FBQyxXQUF4QixDQUFvQyxNQUFwQztFQURnQixDQUFsQjtBQUFBIiwic291cmNlc0NvbnRlbnQiOlsiJCgnLmhpZGVyJykuY2xpY2sgKCktPlxuICAkKEApLnBhcmVudCgnLm1lc3NhZ2UnKS5yZW1vdmVDbGFzcygnYmx1cicpIl19
+//# sourceURL=coffeescript
